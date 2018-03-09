4f3497d011c7686d38413c096a7cdb076f2060ee
Michael Bishop <cleverca22@gmail.com>
2018-03-08 20:06:28 -0400

    add a wee-slack patch that allows connecting without the proper app

---
+++ wee-slack.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+# nix-build wee-slack.nix
+# cp -vif result/wee_slack.py ~/.weechat/python/autoload
+let
+  env = buildEnv {
+    name = "deps";
+    paths = [ python27Packages.websocket_client python27Packages.six ];
+  };
+in stdenv.mkDerivation {
+  name = "wee-slack";
+  src = fetchFromGitHub {
+    owner = "wee-slack";
+    repo = "wee-slack";
+    rev = "221dfc528d7df5460b5d0014d28a6dd6f733ca7b";
+    sha256 = "0712zzscgylprnnpgy2vr35a5mdqhic8kag5v3skhd84awbvk1n5";
+  };
+  patches = [ ./wee_slack.patch ./libpath.patch ];
+  NIX_OUTPUT = "${env}/lib/python2.7/site-packages";
+  installPhase = ''
+    mkdir $out
+    substitute wee_slack.py $out/wee_slack.py --subst-var NIX_OUTPUT
+  '';
+}
